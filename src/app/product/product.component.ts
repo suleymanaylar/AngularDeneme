5435bde4729560cb2066c6f553d1046018c4a872
sloterr <50946629+sloterr@users.noreply.github.com>
2019-11-28 20:47:13 +0300

    Angular

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+  title="Ürün Listesi"
+  products:Product[]=[
+    {id:1,name:"laptop",price:1000,categoryId:1,description:"Asus",imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {id:1,name:"Suleyman",price:2000,categoryId:2,description:"Monster",imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
+    {id:1,name:"Hande",price:2000,categoryId:2,description:"Monster",imageUrl:"https://images.unsplash.com/photo-1516387938699-a93567ec168e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"}
+  ]
+
+  
+  ngOnInit() {
+  }
+
+}
