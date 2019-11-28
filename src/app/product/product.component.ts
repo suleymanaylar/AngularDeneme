7888a92233bea280396c3ffe523ade7544ae3d00
sloterr <50946629+sloterr@users.noreply.github.com>
2019-11-28 21:17:47 +0300

    pipe implement

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,6 +10,7 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title="Ürün Listesi"
+  filterText:string
   products:Product[]=[
     {id:1,name:"laptop",price:1000,categoryId:1,description:"Asus",imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
     {id:1,name:"Suleyman",price:2000,categoryId:2,description:"Monster",imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
