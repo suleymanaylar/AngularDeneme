25cbe4bde4ed278b7a8b5146da05bf7becce8f58
sloterr <50946629+sloterr@users.noreply.github.com>
2019-12-08 20:04:20 +0300

    urun ekleme form tasarımı

--- src/app/product/product-add-forms1/product-add-forms1.component.ts
+++ src/app/product/product-add-forms1/product-add-forms1.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/category.service';
 import { Product } from '../product';
 import { Category } from 'src/app/category/category';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product-add-forms1',
   templateUrl: './product-add-forms1.component.html',
   styleUrls: ['./product-add-forms1.component.css'],
-  providers:[CategoryService]
+  providers: [CategoryService]
 })
 export class ProductAddForms1Component implements OnInit {
 
-  constructor(private categoryService:CategoryService) { }
-  model:Product=new Product();
-  categories:Category[];
+  constructor(private categoryService: CategoryService) { }
+  model: Product = new Product();
+  categories: Category[];
 
   ngOnInit() {
-    this.categoryService.getCategories().subscribe(data=>{
-      this.categories=data;
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
     })
   }
-
+  add(form: NgForm) {
+ console.log(form.value.categoryId);
+  }
 }
