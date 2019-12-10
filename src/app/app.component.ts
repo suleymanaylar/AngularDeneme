36d8d63e6f29c2d1a95dda66724d6cfae7b4d2ed
Suleyman Aylar <50946629+suleymanaylar@users.noreply.github.com>
2019-12-10 16:18:01 +0300

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AcoountService } from './services/acoount.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop';
+
+  constructor(private accountService:AcoountService){}
+  isloggin(){
+    return this.accountService.isloggedin();
+  }
+  logout(){
+    this.accountService.logout();
+  }
 }
+
+
